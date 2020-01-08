d64556eaaead75114cdebf89ebd89189d878d027
RIPinPuppers <leonhard.schnaitl@htl-salzburg.ac.at>
2020-01-08 18:28:19 +0100

    added GameManager script for ball movement and toggle to manage position repeat

---
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Transform[] positions;
+    [SerializeField] private Transform ball;
+    [SerializeField] private bool positionRepeat = true;
+
+    public void MoveBall ()
+    {
+        Vector3 newPos = positions[Random.Range(0, positions.Length)].position;
+        Vector3 lastPos;
+
+        do
+        {
+            lastPos = ball.position;
+            ball.position = newPos;
+            newPos = positions[Random.Range(0, positions.Length)].position;
+        } 
+        while (ball.position == lastPos && !positionRepeat);
+    }
+
+    public void SetRepeat(bool mode)
+    {
+        positionRepeat = mode;
+    }
+}
